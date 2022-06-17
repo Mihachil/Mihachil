2ebc56188ae144582d51a0a450f56618be2986b0
Mihail <nohrin.misha2003@yandex.ru>
2022-06-17 11:38:32 +0500

    1_Zadanie

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        trueButton = findViewById(R.id.true_butthon)
+        falseButton = findViewById(R.id.false_butthon)
+
+        trueButton.setOnClickListener{
+            Toast.makeText(this, R.string.Toast_corect,Toast.LENGTH_LONG).show()
+        }
+
+        falseButton.setOnClickListener{
+            Toast.makeText(this, R.string.Toast_incorrect,Toast.LENGTH_LONG,)
+        }
+
+
+
+
     }
 }
